bff8f9b2561cbdbebd706acd9d79bd917b6c85d4
Varunraje <varunraje47@gmail.com>
2023-07-01 16:59:44 +0530

    Task Adding added

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Todo from './src/Todo';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Todo />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/Todo.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+
+const Todo = () => {
+  const [tasks, settasks] = useState([])
+  const [input, setInput] = useState()
+
+  const AddInput = () => {
+    if (input.length > 0) {
+      const DummyTask = tasks
+      DummyTask.push(input)
+      settasks(DummyTask)
+      setInput('')
+    }
+    console.log(tasks)
+  }
+
+  return (
+    <View>
+      <View>
+        <Text style={styles.heading}>Add Your Task</Text>
+        <TextInput value={input} style={styles.input} placeholder="Add todo"
+          onChangeText={setInput}
+        />
+        <TouchableOpacity onPress={AddInput}>
+          <Text style={styles.button}>Add</Text>
+        </TouchableOpacity>
+      </View>{
+        (tasks.length === 0) ?
+          <Text>NO TASKS</Text> : <Text>{tasks.map((element, index) => {
+            return (
+              <View key={index}>
+                <Text>{element}</Text>
+                <TouchableOpacity>
+                  <Text style={styles.button}>Delete</Text>
+                </TouchableOpacity>
+              </View>
+            )
+          }
+          )}</Text>
+      }
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: 'gray',
+    padding: 10,
+    fontSize: 20,
+  },
+  heading: {
+    fontSize: 40,
+    fontWeight: 'bold'
+  },
+  button: {
+    backgroundColor: 'green',
+  },
+  list: {
+    // flexWrap: 'wrap'
+    marginTop: 20,
+
+
+
+  }
+})
+
+
+export default Todo
